509600196aeec2d505b867d5eb41622c1427e98f
Montazar Al-Jaber <muntjaber@github.com>
2018-10-08 19:33:15 +0200

    Added error checking and moved variables

--- main.c
+++ main.c
@@ -1,44 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "resistance.h"
 #include "libpower.h"
 
 int main()
 {
-    // User form
-    printf("Ange koppling [S|P]:\n");
-    scanf("%c", &conn);
+    char conn_type = 0;
+    unsigned int count = 0;
+    float* output = NULL;
 
-    printf("Antal komponenter:\n");
-    scanf("%d", &count);
-    // End of user form
+    float myResistance;
+    float resistanceS;
+    float resistanceP;
+    float totalResistanceS = 0;
+    float totalResistanceP = 0;
 
+    do {
+        printf("Ange koppling [S|P]:\n");
+        scanf("%1c", &conn_type);
 
-    // Resistance
-    float *array = calloc(count, sizeof(float));
+        conn_type = conn_type.toupper();
 
-    for (i = 0; i < count; i++)
+    } while (conn_type != 'S' || conn_type != 'P');
+
+    do {
+        printf("Antal komponenter:\n");
+        scanf("%d", &count);
+
+    } while (count <= 0);
+
+    output = calloc(count, sizeof(float));
+    if (output) == NULL
+        exit(EXIT_FAILURE);
+
+    for (size_t i = 0; i < count; i++)
     {
         printf("Komponent %d i ohm:\n", i + 1);
         scanf("%f", &myResistance);
         array[i] = myResistance;
     }
     
-    totalResistance = calc_resistance(count, conn, array);
+    totalResistance = calc_resistance(count, conn_type, array);
     // End of resistance
 
 
     // Output
-    printf("Ersättningsresistans: %f ohm\n", totalResistance);
+    printf("Ersï¿½ttningsresistans: %f ohm\n", totalResistance);
     // End of output
 
 
     // Call the power function with value from resistance.
     effect = calc_power_r(volt, totalResistance);
 
-    printf("Effekten är: %f Watt\n", effect;
+    printf("Effekten ï¿½r: %f Watt\n", effect;
    // End of power function
 
+    free(output);
 
-    free(array);
+    return 0;
 }
