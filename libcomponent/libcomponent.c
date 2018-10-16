369bbc484fc4e00b2646fdb69d17c192ac67dcb5
Montazar <26507935+muntjaber@users.noreply.github.com>
2018-10-16 23:33:27 +0200

    Update libcomponent.c

--- libcomponent/libcomponent.c
+++ libcomponent/libcomponent.c
@@ -1,6 +1,6 @@
 #include "libcomponent.h"
 
-//static const float E12[] = {1.0, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2};
+static const float E12[] = {1.0, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2};
 
 /*
 * origo_resistance: Ersättningsresistans
